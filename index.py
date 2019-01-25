e312f00dda74ce96424b2270a9f2c9e11df18566
ruying <164972038@qq.com>
2019-01-25 21:33:23 +0800

    添加视图login

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
 def index(response):
     return HttpResponse('index')
+
+
+def login(response):
+    return redirect('/index')
